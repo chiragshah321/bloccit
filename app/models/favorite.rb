bef1840cb09562eda238a1d14d52b3cc092382ef
Chirag Shah <chiragshah321@gmail.com>
2017-05-05 23:51:14 +0000

    checkpoint32-assignment incomplete

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,24 @@
 class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  def favorite_for(post)
+        favorites.where(post_id: post.id).first
+  end
+  
+  def avatar_url(size)
+      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+  
+  def points
+     votes.sum(:value)
+  end
+   
+  def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+     new_rank = points + age_in_days
+     update_attribute(:rank, new_rank)
+  end
+  
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe Favorite, type: :model do
    
   it { is_expected.to belong_to(:post)}  
   it { is_expected.to belong_to(:user)}  
-    
   
+  describe "favorite by user" do
+    it "should display favorite post" do
+      favorite =  user.favorites.where(post: @post).create
+      expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  
+    it "should displays author's gravatar" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+    end
+  
+    it "should display votes on the post" do
+      expect( post.points ).to eq(@up_votes - @down_votes)
+    end
+  
+    it "should display number of comments" do
+      expect(comment).to have_attributes(body: "Comment Body")
+    end
+  end  
 end
